92aefe556521f3f51d16b5caccc44a2f95e005bd
Muhammad Affan <mhd.affann@gmail.com>
2024-01-20 22:28:52 +0700

    test: add AnimalListHeaderViewTest

---
+++ AnimalLibraryTests/Screens/Animal List/Subviews/AnimalListHeaderViewTest.swift
@@ -0,0 +1,53 @@
+//
+//  AnimalListHeaderViewTest.swift
+//  AnimalLibraryTests
+//
+//  Created by Muhammad Affan on 20/01/24.
+//
+
+@testable import AnimalLibrary
+import XCTest
+
+final class AnimalListHeaderViewTest: XCTestCase {
+    
+    func test_configureUI() {
+        let sut = makeSUT()
+        
+        XCTAssertTrue(sut.contentView.contains(sut.nameLabel))
+        XCTAssertTrue(sut.contentView.contains(sut.chevronButton))
+        XCTAssertTrue(sut.contentView.contains(sut.lineView))
+    }
+    
+    func test_updateUI() {
+        let sut = makeSUT()
+        
+        sut.updateUI(title: "Lion", selected: true)
+        
+        XCTAssertEqual(sut.nameLabel.text, "Lion")
+        XCTAssertEqual(sut.chevronButton.isSelected, true)
+    }
+    
+    func test_tapChevronButton() {
+        let sut = makeSUT()
+        let exp = expectation(description: "waiting for chevronButton tapped")
+        sut.onSelected = { _ in
+            exp.fulfill()
+        }
+        
+        sut.updateUI(title: "Lion", selected: true)
+        XCTAssertEqual(sut.chevronButton.isSelected, true)
+        sut.chevronButton.sendActions(for: .primaryActionTriggered)
+        
+        waitForExpectations(timeout: 0.5) { _ in
+            XCTAssertFalse(sut.chevronButton.isSelected)
+        }
+    }
+    
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> AnimalListHeaderView {
+        var sut = AnimalListHeaderView()
+        trackForMemoryLeaks(sut)
+        
+        return sut
+    }
+    
+}
